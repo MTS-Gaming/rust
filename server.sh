39f242368777df1a4ac5b25c8cee6ea5aa30903a
Andrew St Clair <andrew_stclair95@hotmail.com>
2022-08-14 13:09:34 +1000

    Update server.sh

--- server.sh
+++ server.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-export LD_LIBRARY_PATH=:`dirname $0`/RustDedicated_Data/Plugins/x86_64
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:`dirname $0`/RustDedicated_Data/Plugins:`dirname $0`/RustDedicated_Data/Plugins/x86_64
 
 ./RustDedicated \
     +rcon.web 1 \
